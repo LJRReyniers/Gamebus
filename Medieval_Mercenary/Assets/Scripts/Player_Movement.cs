7f155430e17218e31d24aa204ce6fbd429b82880
LJRReyniers <93527848+LJRReyniers@users.noreply.github.com>
2025-03-25 15:29:14 +0100

    ✨ Added sword attack

--- Medieval_Mercenary/Assets/Scripts/Player_Movement.cs
+++ Medieval_Mercenary/Assets/Scripts/Player_Movement.cs
@@ -4,17 +4,35 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed;
-
     [SerializeField] private float _rotationSpeed;
+    [SerializeField] private Animator _animation;
 
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
+    private InputAction _attackAction;
+
+    private bool _attackButtonPressed = false;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _attackAction = InputSystem.actions.FindAction("Attack");
+    }
+
+    private void Update()
+    {
+        if (_attackButtonPressed)
+        {
+            _animation.Play("Slash");
+        }
+
+        OnAttack(_attackAction);
     }
 
     private void FixedUpdate()
@@ -48,5 +66,11 @@ public class Movement : MonoBehaviour
     private void OnMove(InputValue inputValue)
     {
         _movementInput = inputValue.Get<Vector2>();
+    }
+
+    private void OnAttack(InputAction inputAction)
+    {
+        _attackButtonPressed = inputAction.IsPressed();
+        _animation.SetBool("Attack_Button_Pressed", inputAction.IsPressed());
     }
 }
