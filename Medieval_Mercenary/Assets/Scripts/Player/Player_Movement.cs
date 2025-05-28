2e779d3cef36260402704f5272bbc7d7b232b438
LJRReyniers <93527848+LJRReyniers@users.noreply.github.com>
2025-05-28 10:21:10 +0200

    🚸 Removed smoothdamp movement

--- Medieval_Mercenary/Assets/Scripts/Player/Player_Movement.cs
+++ Medieval_Mercenary/Assets/Scripts/Player/Player_Movement.cs
@@ -24,20 +24,22 @@ public class Player_Movement : MonoBehaviour
 
     private void SetPlayerVelodity()
     {
-        _smoothedMovementInput = Vector2.SmoothDamp(
+        /*_smoothedMovementInput = Vector2.SmoothDamp(
             _smoothedMovementInput,
             _movementInput,
             ref _movementInputSmoothVelocity,
             0.1f);
 
-        _rigidbody.linearVelocity = _smoothedMovementInput * _speed;
+        _rigidbody.linearVelocity = _smoothedMovementInput * _speed;*/
+        _rigidbody.linearVelocity = _movementInput * _speed;
     }
 
     private void RotateInDirectionOfInput()
     {
         if (_movementInput != Vector2.zero)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            //Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _movementInput);
             Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime);
 
             _rigidbody.MoveRotation(rotation);
